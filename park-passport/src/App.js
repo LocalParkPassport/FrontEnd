7ff2b1138c3d0e9938fc168dbdb9d49276589f56
Suffyan Tariq <suffyan.tariq@gmail.com>
2019-11-16 09:13:16 -0800

    Created-React-App and initial setup

---
+++ park-passport/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div><h1>Parks Passport Application</h1></div>
+      <h3>Search Feature</h3>
+      {/* <Search /> */}
+      <h3>Cards Display</h3>
+      {/* <Cards /> */}
+    </div>
+  );
+}
+
+export default App;
