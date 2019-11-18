45000f13dfd69278603b7feadc7128b2ed04563d
dantebui <senpaicaboose@gmail.com>
2019-11-18 16:26:38 -0700

    Login and Signup sending data to backend

--- park-passport/src/App.js
+++ park-passport/src/App.js
@@ -5,15 +5,18 @@ import './App.css';
 // Import Components here
 import ParkList from './Components/ParkList'
 import AppBar from './Components/AppBar'
-
+import Login from './Components/Login'
+import Signup from './Components/Signup';
 
 // Application here
 function App() {
   return (
     <div className="App">
       <div><h1>Parks Passport Application</h1></div>
-      {<AppBar    />}
-      {<ParkList  />}
+      {/* {<AppBar    />}
+      {<ParkList  />} */}
+      <Login/>
+      <Signup/>
     </div>
   );
 }
