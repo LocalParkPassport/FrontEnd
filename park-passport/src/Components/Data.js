2686b66c2c4cac13c520121eeb937a56eba8b0c7
Suffyan Tariq <suffyan.tariq@gmail.com>
2019-11-19 14:13:30 -0800

    added Park Card Component

---
+++ park-passport/src/Components/Data.js
@@ -0,0 +1,37 @@
+export const data=[
+{
+    "name": "new park",
+    "description": "lots of trees",
+    "location": "lagos",
+    "wildlife": true,
+    "dog_park": false,
+    "hiking_trails": false,
+    "disc_golf": false,
+    "open_spaces": false,
+    "climbing trees": false,
+    "user_id": 1
+},
+{
+    "name": "new park",
+    "description": "lots of trees",
+    "location": "lagos",
+    "wildlife": false,
+    "dog_park": false,
+    "hiking_trails": false,
+    "disc_golf": false,
+    "open_spaces": false,
+    "climbing trees": false,
+    "user_id": 2
+},
+{
+    "name": "new park",
+    "description": "lots of trees",
+    "location": "lagos",
+    "wildlife": true,
+    "dog_park": false,
+    "hiking_trails": false,
+    "disc_golf": false,
+    "open_spaces": false,
+    "climbing_trees": false,
+    "user_id": 3
+},]
